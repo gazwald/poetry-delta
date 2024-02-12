e79804994bf6d4dab637d14590920300216e6ca3
ZRGX <139933536+RGX650@users.noreply.github.com>
2024-02-12 12:57:39 +0100

    correction

--- delta/cli/base.py
+++ delta/cli/base.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 import click
 from process.repo import ProcessRepo
 from process import backup
@@ -20,12 +20,12 @@ backup_dir = os.path.join(os.getenv('POETRY_HOME'), "backup")
 @click.option("--autosave", is_flag=True, help="Use last autosave")
 
 def handle_local_project(
-    path: Path, branch: str, package: str, rev: str, local: bool, use_backup: bool, use_autosave: bool):
+    path: Path, package: str | None = None, branch: str, local: bool, use_backup: bool, use_autosave: bool, backup_dir):
     # Define current_version and previous_versions source type
     if use_backup:
-        get_last_poetry_lock = get_last_backup()
+        get_last_poetry_lock = get_last_backup(backup_dir)
     elif use_autosave:
-        get_last_poetry_lock = get_autosave()
+        get_last_poetry_lock = get_autosave(backup_dir)
 
     # Load the current and previous versions of the files
     current_versions = load_file(os.path.join(find_pyproject_path(), "poetry.lock"))
@@ -46,13 +46,14 @@ def main(
 ):
     repo = ProcessRepo._fetch_repo(path)
     if repo is None:
-        handle_local_project(path, package)
+        if package is not None:
+            message.append(f"Filtering on package {package}")
+        click.echo("\n".join(message))
+        handle_local_project(path, branch, package, local, backup, autosave)
         
     else:
         message: list[str] = [f"Inspecting branch '{branch}'"]
         if package is not None:
             message.append(f"Filtering on package {package}")
-    
         click.echo("\n".join(message))
-    
         ProcessRepo(path, branch, package, rev)
